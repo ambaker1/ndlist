9e5c60e2e5ae73fa694a9579dadd92ea00f23aa9
Alex Baker <ambaker1@mtu.edu>
2023-10-11 17:32:48 -0500

    Updated build file

--- build.tcl
+++ build.tcl
@@ -13,12 +13,9 @@ namespace import ndlist::*
 tin import flytrap
 # Source all test files
 source tests/core_test.tcl
-exit
-
+source tests/stat_test.tcl
 source tests/linalg_test.tcl
 source tests/ltools_test.tcl
-source tests/mathfunc_test.tcl
-source tests/stat_test.tcl
 
 # Check number of failed tests
 set nFailed $::tcltest::numTests(Failed)
