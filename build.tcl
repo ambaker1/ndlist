3d1200ec50ff448215a4574f5d44e17cd5464f71
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-10-14 14:04:19 -0500

    Update build.tcl
    Added doc examples and documentation pdf into build file.

--- build.tcl
+++ build.tcl
@@ -4,17 +4,22 @@ tin import tcltest
 set version 0.3
 set config ""
 dict set config VERSION $version
+
+puts "Building from source files..."
 tin bake src build $config
 tin bake doc/template/version.tin doc/template/version.tex $config
+source tests/build_examples.tcl
 
-# Import ndlist
+puts "Loading package from build folder..."
 source build/ndlist.tcl 
 namespace import ndlist::*
-tin import flytrap
+
 # Source all test files
+puts "Running all tests..."
 source tests/vector_test.tcl
 source tests/matrix_test.tcl
 source tests/tensor_test.tcl
+source tests/examples.tcl 
 
 # Check number of failed tests
 set nFailed $::tcltest::numTests(Failed)
@@ -26,12 +31,20 @@ cleanupTests
 if {$nFailed > 0} {
     error "$nFailed tests failed"
 }
+
+puts "Tests passed, installing..."
 # Tests passed, copy build files to main folder and install
 file copy -force {*}[glob -directory build *] [pwd]
-
 exec tclsh install.tcl
 
 # Verify installation
+puts "Verifying installation..."
 tin forget ndlist
 tin clear
 tin import ndlist -exact $version
+
+# Build documentation
+puts "Building documentation..."
+cd doc
+exec -ignorestderr pdflatex ndlist.tex
+cd ..
